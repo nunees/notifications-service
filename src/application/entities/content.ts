0b86f5113275343330eddd1f83c8369fcc247ba1
nunees <felipe_silva_nunes@hotmail.com>
2023-01-31 18:55:39 -0300

    Fix: remove code that was not correct and fix issue

---
+++ src/application/entities/content.ts
@@ -0,0 +1,20 @@
+export class content{
+  private readonly content: string;
+
+  get value(): string{
+    return this.content;
+  }
+
+  private validateContentLength(content: string): boolean{
+    return content.length >= 5 && content.length <= 248;
+  }
+
+  constructor(content: string){
+    const isContentLengthValid = this.validateContentLength(content);
+    if(!isContentLengthValid){
+      throw new Error("Content is invalid");
+    }
+
+    this.content = content;
+  }
+}
